2552a08ea9e5e1b8da2b08c746aa1c193ed32fa3
apolloandroid <hmelevskih94@gmail.com>
2021-07-16 03:02:23 +0300

    Fragment for lessons history showing

---
+++ app/src/main/java/com/example/fitnesskittest/view/lessons/LessonsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.fitnesskittest.view.lessons
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fitnesskittest.R
+
+
+class LessonsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lessons, container, false)
+    }
+}
